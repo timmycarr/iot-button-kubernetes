5f6a18a787a1f799ed3bd93018ca0076bd5b2aec
Tim Carr <tim@timcarr.net>
2018-03-20 07:58:10 -0500

    added launch-cfn.py

---
+++ launch-cfn.py
@@ -0,0 +1,53 @@
+import boto3
+
+cfn = boto3.client('cloudformation',)
+
+response = cfn.create_stack(
+    StackName='Heptio-QS-Kubernetes-2',
+    TemplateURL='https://s3.amazonaws.com/quickstart-reference/heptio/latest/templates/kubernetes-cluster-with-new-vpc.template',
+    Parameters=[
+        {
+            "ParameterKey": "AvailabilityZone",
+            "ParameterValue": "us-west-2b"
+        },
+        {
+            "ParameterKey": "AdminIngressLocation",
+            "ParameterValue": "0.0.0.0/0"
+        },
+        {
+            "ParameterKey": "KeyName",
+            "ParameterValue": ""
+        },
+        {
+            "ParameterKey": "NetworkingProvider",
+            "ParameterValue": "calico"
+        },   
+        {
+            "ParameterKey": "K8sNodeCapacity",
+            "ParameterValue": "2"
+        },
+        {
+            "ParameterKey": "InstanceType",
+            "ParameterValue": "m4.large"
+        },
+        {
+            "ParameterKey": "DiskSizeGb",
+            "ParameterValue": "40"
+        },
+        {
+            "ParameterKey": "BastionInstanceType",
+            "ParameterValue": "t2.micro"
+        },
+        {
+            "ParameterKey": "QSS3BucketName",
+            "ParameterValue": "quickstart-reference"
+        },
+        {
+            "ParameterKey": "QSS3KeyPrefix",
+            "ParameterValue": "heptio/latest"
+        },
+    ],
+    OnFailure='ROLLBACK',
+    EnableTerminationProtection=False
+)
+print(response)
